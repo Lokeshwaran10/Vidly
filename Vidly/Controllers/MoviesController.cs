7ddbce2577a47f4a7213d2200317a74c3b022864
Lokeshwaran <officiallokesh96@gmail.com>
2020-12-11 21:09:17 +0530

    Added Customers and Movies -Section_2

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,31 +10,37 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random 
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+        // GET: Movies/Random
+
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer { Name = "Customer 1"},
-                new Customer { Name = "Customer 2"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
-
             var viewModel = new RandomMovieViewModels
             {
                 Movie = movie,
                 Customers = customers
             };
-                
-
             return View(viewModel);
-            
-        } 
-       
-        //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
-        // public ActionResult ByReleaseDate(int year, int month)
-        //{
-        //     return Content(year + "/" + month);    
-        //}
+        }
     }
 }
